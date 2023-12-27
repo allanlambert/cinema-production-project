db1647a68d4c892382355591d6da13e117c0d3e0
Vyybor <vyybor@gmail.com>
2023-12-26 23:22:48 -0500

    final touch up

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,22 +8,35 @@ function Footer() {
       <footer id="footer">
         <div className="footer__wrapper">
           <figure className="footer__logo--wrapper">
-            <Link  to='/'>
-            <img src={logo} alt="logo" className="footer__logo" />
+            <Link to="/">
+              <img src={logo} alt="logo" className="footer__logo" />
             </Link>
           </figure>
           <ul className="footer__links--wrapper">
             <li className="footer__links">
-                <Link to='/' className="footer__link">Home</Link>
+              <Link to="/" className="footer__link">
+                Home
+              </Link>
             </li>
             <li className="footer__links">
-                <a href="#" className="footer__link not-allowed">About</a>
+              <a href="#" className="footer__link not-allowed">
+                About
+              </a>
             </li>
             <li className="footer__links">
-                <Link to='/explore' className="footer__link">Explore</Link>
+              <Link to="/explore" className="footer__link">
+                Explore
+              </Link>
             </li>
           </ul>
-          <span className="footer__terms">Terms of Use | Privacy Policy</span>
+          <div className="footer__terms">
+            <a href="" className="footer__term">
+              Terms of Use
+            </a><span className="footer-line">|</span>
+            <a href="" className="footer__term">
+              Privacy Policy
+            </a>
+          </div>
         </div>
       </footer>
     </>
